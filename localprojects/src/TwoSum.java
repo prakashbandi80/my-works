408a6e2a12d29dd0a285417b62b5835e4bf317ee
Prakash Reddy Bandi <prakash.bandi@gmail.com>
2020-06-27 23:38:42 -0700

    comments added

--- localprojects/src/TwoSum.java
+++ localprojects/src/TwoSum.java
@@ -1,7 +1,11 @@
 import java.util.Arrays;
 
 public class TwoSum {
-
+    /*
+      Given nums = [2, 7, 11, 15], target = 9,
+      Because nums[0] + nums[1] = 2 + 7 = 9,
+      return [0, 1].
+     */
     public static void main(String[] args) {
 
         int[] numberArray = {2, 7, 11, 15};
